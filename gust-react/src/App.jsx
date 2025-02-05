6cb743e3a0e6a9f2ac700fb714bdc5fba5ab65bc
Jacob-Stock <jacobstock88@gmail.com>
2025-02-04 20:36:53 -0600

    Leaflet maps

--- gust-react/src/App.jsx
+++ gust-react/src/App.jsx
@@ -1,43 +1,19 @@
-import { useState } from "react";
+import React from "react";
+import MapComponent from "./Components/MapComponent.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div style={styles.container}>
-      <h1 style={styles.header}>My test React App!</h1>
-      <p style={styles.text}>Click the button to increase the count:</p>
-      <button style={styles.button} onClick={() => setCount(count + 1)}>
-        Count: {count}
-      </button>
+    <div style = {{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw",
+    }}>
+      <MapComponent />
     </div>
   );
 }
 
-const styles = {
-  container: {
-    textAlign: "center",
-    padding: "50px",
-    fontFamily: "Arial, sans-serif",
-    backgroundColor: "#282c34",
-    color: "white",
-    minHeight: "100vh",
-  },
-  header: {
-    fontSize: "2.5rem",
-  },
-  text: {
-    fontSize: "1.2rem",
-  },
-  button: {
-    padding: "10px 20px",
-    fontSize: "1.5rem",
-    backgroundColor: "#61dafb",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-    marginTop: "10px",
-  },
-};
 
 export default App;
