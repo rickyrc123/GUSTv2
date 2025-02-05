7064ab0d0ddd1a451ba698bca2f3493497606478
NickWarren317 <yoitsbiggiecheese@gmail.com>
2025-02-05 13:06:14 -0600

    Drone creation and position posting in fastAPI

--- db/models.py
+++ db/models.py
@@ -1,6 +1,6 @@
 import enum
 
-from sqlalchemy import Column, Double, Enum, ForeignKey, Integer, String, TIMESTAMP, Boolean, text
+from sqlalchemy import Column, Double, Enum, ForeignKey, Integer, String, TIMESTAMP, Boolean, text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -13,24 +13,36 @@ class DroneModels(enum.Enum):
 class Post(Base):
   __tablename__ = 'posts'
 
-  id = Column(Integer, primary_key=True, nullable=False)
-  title = Column(String, nullable=False)
-  content = Column(String, nullable=False)
-  published = Column(Boolean, server_default='TRUE')
-  created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+  id          = Column(Integer, primary_key=True, nullable=False)
+  title       = Column(String, nullable=False )
+  content     = Column(String, nullable=False)
+  published   = Column(Boolean, server_default='TRUE')
+  created_at  = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
 
 # Drone information table
 class Drone(Base):
   __tablename__ = 'drones'
 
-  id = Column(Integer, primary_key=True, nullable=False)
-  name = Column(String, unique=True)
-  longitude = Column(Double, nullable=False)
-  latitude = Column(Double, nullable=False)
-  altitude = Column(Double, nullable=False)
-  direction = Column(Double, nullable=False)
-  model = Column(Enum(DroneModels), nullable=False)
-  # state = Column(Enum, nullable=False, server_default="off")
+  id        = Column(Integer, primary_key=True, nullable=False        )
+  name      = Column(String, unique=True                              )
+  last_long = Column(Double, nullable=True                            )
+  last_lat  = Column(Double, nullable=True                            )
+  last_alt  = Column(Double, nullable=True                            )
+  last_dir  = Column(Double, nullable=True                            )
+  model     = Column(String, nullable=True                            )
+  state     = Column(Integer, nullable=False,  server_default="0"     )
+  created_at= Column(DateTime, nullable=False, server_default='now()' )
+
+class DronePositions(Base):
+  __tablename__ = 'positions'
+  
+  id        = Column(Integer, primary_key=True, nullable=False                          )
+  longitude = Column(Double, nullable=False                                             )
+  latitude  = Column(Double, nullable=False                                             )
+  altitude  = Column(Double, nullable=False                                             )
+  direction = Column(Double, nullable=False                                             )
+  timestamp = Column(DateTime, primary_key=True, nullable=False, server_default='now()' )
+
 
 class Swarm(Base):
   __tablename__ = 'swarms'
@@ -48,8 +60,8 @@ class Drone_Swarms(Base):
 class Program(Base):
   __tablename__ = 'programs'
 
-  id = Column(Integer, primary_key=True, nullable=False)
-  title = Column(String, nullable=False)
-  content = Column(String, nullable=False)
-  created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
-  last_updated = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+  id            = Column(Integer, primary_key=True, nullable=False)
+  title         = Column(String, nullable=False)
+  content       = Column(String, nullable=False)
+  created_at    = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+  last_updated  = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
