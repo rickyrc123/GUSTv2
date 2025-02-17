ba6c6f7c2099c13a024571226c1ee0a1e907d21d
Jammont3 <jmmcubed@gmail.com>
2025-02-16 19:47:22 -0600

    Hot fix
    - Use paths for includes within db

--- db/setup.py
+++ db/setup.py
@@ -1,7 +1,7 @@
 #Once the database is setup, this will create all the tables defined in models
 import os
 from sqlalchemy import create_engine
-from models import *
+from .models import *
 
 def build():
     engine = create_engine(os.getenv("DATABASE_URL"))
