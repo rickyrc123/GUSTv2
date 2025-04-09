ed3b1d6e3fe3671e6881b01b28ae8fdb796bd703
Ricardo R-C <raroucocrenshaw@crimson.ua.edu>
2025-04-08 21:13:59 -0500

    styling changes

--- gust-react/src/Components/PlanningWidget.jsx
+++ gust-react/src/Components/PlanningWidget.jsx
@@ -85,7 +85,7 @@ const PlanningWidget = () => {
       </div>
 
       <div className="map-container">
-        <MapContainer center={mapCenter} zoom={13} style={{ height: '100%', width: '100%' }}>
+        <MapContainer center={mapCenter} zoom={13} style={{ height: '100%', width: '80%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
